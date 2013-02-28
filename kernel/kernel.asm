59b0ddbb9bcfeb96e12120f9901e8553d0e12ec1
C0reFast <topchenfu@qq.com>
2013-02-28 21:44:56 +0800

    expand the kernel

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -2,13 +2,31 @@
 ;	kernel.asm
 ;===========================================================================
 
+SELECTOR_KERNEL_CS	equ	8
 
+extern	cstart	;导入函数
+extern	gdt_ptr	;导入全局变量
+
+[section .bss]
+StackSpace	resb	2 * 1024
+StackTop	;栈顶
+
+	
 [section .text]	;
 
-global	_start	;
+global	_start	;导出_start
 
 _start:
-	mov	ah, 0Fh
-	mov	al, 'K'
-	mov	[gs:((80 * 1 + 39) * 2)], ax
-	jmp 	$
+	mov	esp, StackTop	;堆栈在bss段中，将esp从Loader移到Kernel
+
+	sgdt	[gdt_ptr]
+	call	cstart
+	lgdt	[gdt_ptr]
+
+	jmp	SELECTOR_KERNEL_CS:csinit
+
+csinit:
+	push	0
+	popfd
+
+	hlt
